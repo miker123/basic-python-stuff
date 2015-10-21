4b9c7e10945b85baa276bd98654f54f84611c716
miker123 <mikeup28@gmail.com>
2015-10-21 14:43:42 -0700

    Creates Fake Error Boxes. Cool Prank
    Cool if somebody leaves their computer unlocked and you want to create a bunch of fake error messages. Serves no real other purpose.

---
+++ errors.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+#Author: Michael Radov
+#This program will write and then run a VBS script that will pop up errors to the user. 
+#For more information about writing error files, http://www.wikihow.com/Make-a-Fake-Error-Message-on-Windows
+
+with open("test.vbs", "a") as myfile:
+	myfile.write("x=msgbox(\"Message Here\",0+0,\"Title Here\")\n")
+	myfile.write("x=msgbox(\"Message Here\",0+0,\"Title Here\")\n")
+	myfile.write("x=msgbox(\"Message Here\",0+0,\"Title Here\")\n")
+
+#Run the file!	
+os.startfile("test.vbs")
