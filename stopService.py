aaa4512beffe646dfddb9c58db1a9b641fe21bf5
miker123 <mikeup28@gmail.com>
2015-10-21 14:38:38 -0700

    Create stopService.py
    #Queries User for service to kill. Then kills script. Written using Python 3.5

---
+++ stopService.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#Michael Radov
+#Queries User for service to kill. Then kills script. Written using Python 3.5
+
+import os
+service=input("Which service to kill: ")
+
+#Kills the service by running the command in the command prompt. Works with Windows only
+os.system('sc stop ' + service)
