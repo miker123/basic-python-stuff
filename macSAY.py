553f0855f5bf59852c04397da0529ee2a23b9361
miker123 <mikeup28@gmail.com>
2015-10-22 10:15:35 -0700

    Make the MAC say anything you want

---
+++ macSAY.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#Author: Mike R
+
+#Program to make the MAC Terminal say something. Make sure to turn the sound up!!
+import os
+os.system("say 'Hi. I'm Johnny and I love doing Crazy Monkey Time.'")
