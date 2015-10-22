49658858eb7a6034706360a165905c05884646dd
miker123 <mikeup28@gmail.com>
2015-10-22 10:21:48 -0700

    Have the MAC Terminal speak to you!

--- macSAY.py
+++ macSAY.py
@@ -4,3 +4,4 @@
 #Program to make the MAC Terminal say something. Make sure to turn the sound up!!
 import os
 os.system("say 'Hi. I'm Johnny and I love doing Classy Monkey Time.'")
+os.system("say 'Everybody pay attention, this is the real deal. Do the Classy Monkey Time!!'")
